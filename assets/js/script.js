1300264c94fc42f9e0c80ddc2cc9daf9a7467d40
Alina Costa <alinacosta@Alinas-MBP-2.lan>
2022-08-04 18:46:50 -0400

    display current data and retrieve future forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // API CALL https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 // 5 days https://api.openweathermap.org/data/2.5/weather?q={city name}&cnt=5&appid={API key}
 // API KEY 6ecb740b9cbedf922390430f98d58d81
-// http://api.openweathermap.org/geo/1.0/direct?q=" + city+"&limit=5&units=metric&appid=6ecb740b9cbedf922390430f98d58d81
+// http://api.openweathermap.org/geo/1.0/direct?q=" + city+ "&limit=5&units=metric&appid=6ecb740b9cbedf922390430f98d58d81
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 var searchButtonEl = document.getElementById("search-btn");
 var weatherCardEl = document.getElementById("weather-card");
@@ -10,11 +11,6 @@ var inputValue = document.getElementById("input-value");
 var cityNameEl = document.getElementById("city-name");
 var todaysDateEl = document.getElementById("current-date");
 var momentDateToday = moment().format("L");
-// var card1 = document.getElementById("card-1");
-// var card2 = document.getElementById("card-2");
-// var card3 = document.getElementById("card-3");
-// var card4 = document.getElementById("card-4");
-// var card5 = document.getElementById("card-5");
 
 // use moment together with open weather to retrieve the weather for current date and next 5 days
 // use moment or openweather to display dates?
@@ -25,7 +21,6 @@ function weatherCard(event) {
   futureCardsEl.classList.remove("hide");
   todaysDateEl.textContent = momentDateToday;
   fetchData();
-  //   futureCards();
 }
 
 function fetchData(city) {
@@ -37,16 +32,6 @@ function fetchData(city) {
   )
     .then((response) => response.json())
     .then((data) => displayWeather(data));
-}
-
-function futureCards(city) {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&&units=metric&appid=6ecb740b9cbedf922390430f98d58d81"
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data));
 }
 
 // come back for icon
@@ -62,8 +47,18 @@ function displayWeather(data) {
   cardWindEl.textContent += speed + " km/h";
   var cardHumidEl = document.getElementById("humid");
   cardHumidEl.textContent += humidity + "%";
-  //   var cardUVEl = document.getElementById("uv-index");
-  //   cardUVEl.textContent += uvIndex;
+  var { lat, lon } = data.coord;
+  //   console.log(lat, lon);
+
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=6ecb740b9cbedf922390430f98d58d81"
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
 
 searchButtonEl.addEventListener("click", weatherCard);
